7428f2c97f82a478a61665be42151975ecb1a7c3
parkjhun00 <parkjhun00@gmail.com>
2023-06-30 15:49:24 +0900

    update部分改善、キャリブレーション追加

--- plotting_v5.py
+++ plotting_v5.py
@@ -19,10 +19,10 @@ LinearRegression(copy_X=True, fit_intercept=True, n_jobs=None)
 
 # Set up the server
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(("localhost", 12345))  # Must match the C++ program
+server_socket.bind(("localhost", 12345))  
 server_socket.listen(1)
 
-fz_values = deque([-250]*9600, maxlen=9600)
+fz_values = deque([0]*9600, maxlen=9600)
 t = np.linspace(-8, 0, 9600) 
 
 #get sensor data
@@ -50,8 +50,8 @@ win.setWindowTitle('Force Visualize')
 
 pg.setConfigOptions(antialias=True)
 
-p1 = win.addPlot(title="Push & Pull 10s")
-p1.setYRange(-300, -200)
+p1 = win.addPlot(title="Push & Pull 8s")
+p1.setYRange(100, -100)
 curve = p1.plot()
 ptr = 0
 def update1():
@@ -63,7 +63,7 @@ def update1():
 p2 = win.addPlot(title="Push & Pull")
 p2.setDownsampling(mode='peak')
 p2.setClipToView(True)
-p2.setYRange(-300, -200)
+p2.setYRange(200, -200)
 curve2 = p2.plot()
 
 data2 = np.empty(100)
@@ -71,15 +71,16 @@ ptr2 = 0
 
 def update2():
     global data2, ptr2
-    while new_value == True:
+    try:
         data2[ptr2] = cali_new_value[0]
-        #data2[ptr2] = np random.normal()
         ptr2 += 1
         if ptr2 >= data2.shape[0]:
             tmp = data2
             data2 = np.empty(data2.shape[0] * 2)
             data2[:tmp.shape[0]] = tmp
         curve2.setData(data2[:ptr2])
+    except NameError:
+        pass
 
 def update():
     update1()
